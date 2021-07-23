c10ec2be7f5837735bdca1108b3b571412bc1b7a
Oakley <cqfangchaoit@sina.com>
2021-07-24 06:36:29 +0800

    update

--- spring-mvc/src/main/java/com/taocaicai/mvc/controller/MyController.java
+++ spring-mvc/src/main/java/com/taocaicai/mvc/controller/MyController.java
@@ -1,15 +1,18 @@
 package com.taocaicai.mvc.controller;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-/**
- * @author Administrator
- */
+import javax.servlet.http.HttpServletRequest;
+
+/** @author Administrator */
 @Controller
 public class MyController {
 
+  /** @return */
   @RequestMapping("/test.do")
   public ModelAndView index() {
     ModelAndView modelAndView = new ModelAndView();
@@ -26,5 +29,26 @@ public class MyController {
     modelAndView.addObject("func", "home");
     modelAndView.setViewName("test");
     return modelAndView;
+  }
+
+  /**
+   * @param request
+   * @return
+   */
+  @RequestMapping("/ip")
+  @ResponseBody
+  public String getRemoteIP(HttpServletRequest request) {
+    String ip = request.getHeader("x-forwarded-for");
+    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+      ip = request.getHeader("Proxy-Client-IP");
+    }
+    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+      ip = request.getHeader("WL-Proxy-Client-IP");
+    }
+    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+      ip = request.getRemoteAddr();
+    }
+    System.out.println("用户请求的IP地址:\t" + ip);
+    return ip;
   }
 }
